8f9ed06b48552aa9a79738655fbceae0570c3ddc
Bingcheng Ni <nibingcheng@johndeere.com>
2021-01-26 21:50:09 -0600

    Commit 2 - Connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 
+const pokemon = require("./models/pokemon.js");
+
+//middleware
+app.use((req, res, next) => {
+    console.log('I run for all routes');
+    next();
+});
+
+app.get('/pokemon', function(req, res) {
+    console.log(pokemon);
+    res.render('show.ejs', {pokemon: pokemon});
+});   
 
 app.listen(3000, () => {
     console.log('I am listening');
