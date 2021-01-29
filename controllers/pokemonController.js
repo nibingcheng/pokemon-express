ecf716fcfdbae04c4f9c20be703cee649bf71776
Bingcheng Ni <nibingcheng@johndeere.com>
2021-01-28 19:13:34 -0600

    Day2 CRUD stuff, I missed it yesterday

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -7,6 +7,34 @@ router.get('/', function(req, res) {
     res.render('index.ejs', {pokemon: pokemon});
 });   
 
+//New
+router.get('/new', (req,res) => {
+    res.render('new.ejs');
+})
+//POST pokemon
+router.post("/", (req,res)=>{
+    pokemon.push(req.body);
+    // console.log(req.body);
+    res.redirect('/pokemon');
+})
+
+//Edit
+router.get('/:index/edit', (req, res)=> {
+    console.log(pokemon[req.params.index]);
+	res.render(
+		'edit.ejs', //render views/edit.ejs
+		{ //pass in an object that contains
+			pokemon: pokemon[req.params.index], 
+			index: req.params.index 
+		}
+	);
+});
+//PUT
+router.put('/:index', (req, res) => { 
+    pokemon[req.params.index] = req.body; 
+	res.redirect('/'); 
+});
+
 //show
 router.get('/:id', function(req, res) {
     let index = req.params.id;
--- server.js
+++ server.js
@@ -1,35 +1,19 @@
 const express = require('express');
 const app = express();
-
-const pokemon = require("./models/pokemon.js");
+const methodOverride = require('method-override');
 
 //middleware start
-app.use((req, res, next) => {
-    console.log('I run for all routes');
-    next();
-});
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({extended:true}));
+
 app.use(express.static('public'));
 app.use('/pokemon', require('./controllers/pokemonController'));
 //middleware end
 
-//index
-// app.get('/pokemon', function(req, res) {
-//     res.render('index.ejs', {pokemon: pokemon});
-// });   
 
-//route for every pokemon
-app.get('/pokemon', function(req, res) {
-    // console.log(pokemon);
-    res.send(pokemon);
+app.get('/', function(req, res) {
+    res.redirect('/pokemon');
 });   
-
-//route for individual pokemon
-// app.get('/pokemon/:id', function(req, res) {
-//     let index = req.params.id;
-//     // res.send(pokemon[index]);
-//     res.render('show.ejs', {pokemon: pokemon[index]})
-// });
-
 
 app.listen(3000, () => {
     console.log('I am listening');
